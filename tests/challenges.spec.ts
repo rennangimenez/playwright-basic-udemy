d6f4028f6d7d1b33314513886d481da815a0cd5c
RGColozzo <rennan.colozzo18@hotmail.com>
2024-10-22 23:56:08 -0300

    adding udemy solutions to challenges tests

--- tests/challenges.spec.ts
+++ tests/challenges.spec.ts
@@ -5,21 +5,37 @@ import { expect, test } from "@playwright/test";
 // 1 - Success Login
 // user - standard_user
 // verify page URL
-// verify at least 1 iten at the final page (visible)
+// verify at least 1 item at the final page (visible)
 test('challenge 1', async({ page }) => {
     await page.goto('https://www.saucedemo.com/v1/');
-    await expect(page).toHaveURL('https://www.saucedemo.com/v1/');
 
     await page.getByTestId('username').fill('standard_user');
     await page.getByTestId('password').fill('secret_sauce');
 
-    const btnLogin = page.locator('input#login-button');
-    btnLogin.click();
+    await page.locator('input#login-button').click();
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/v1/inventory.html');
 
     const txtImgBackpack = page.locator('div.inventory_item_name').nth(0);
-
     await expect(txtImgBackpack).toBeVisible();
     await expect(txtImgBackpack).toHaveText('Sauce Labs Backpack');
+
+    /*
+
+    Udemy solution:
+    
+    await page.goto('https://www.saucedemo.com/v1/');
+
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('input#login-button').click();
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/v1/inventory.html');
+    
+    const productsLabel = page.locator('div.product_label');
+    await expect(productsLabel).toHaveText('Products');
+
+    */
 })
 
 // 2 - Login with locked user
@@ -31,13 +47,32 @@ test('challenge 2', async({ page }) => {
     await page.getByTestId('username').fill('locked_out_user');
     await page.getByTestId('password').fill('secret_sauce');
 
-    const btnLogin = page.locator('input#login-button');
-    btnLogin.click();
+    await page.locator('input#login-button').click();
 
     const txtErrorMessage = page.getByTestId('error');
 
     await expect(txtErrorMessage).toBeVisible();
     await expect(txtErrorMessage).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+
+    /*
+
+    Udemy solution:
+    
+    const txtErrorMessage = page.getByTestId('error');
+
+    await page.goto('https://www.saucedemo.com/v1/');
+
+    await expect(errorLabel).not.toBeVisible();
+
+    await page.locator('[data-test="username"]').fill('locked_out_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('input#login-button').click();
+   
+    const errorLabel = page.locator('[data-test="error"]');
+    await expect(errorLabel).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+    await expect(errorLabel).toBeVisible();
+
+    */
 })
 
 // 3 - Login error - wrong password
@@ -48,11 +83,26 @@ test('challenge 3', async({ page }) => {
     await page.getByTestId('username').fill('standard_user');
     await page.getByTestId('password').fill('wrongpassword');
 
-    const btnLogin = page.locator('input#login-button');
-    btnLogin.click();
+    await page.locator('input#login-button').click();
 
     const txtErrorMessage = page.getByTestId('error');
 
     await expect(txtErrorMessage).toBeVisible();
     await expect(txtErrorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
+
+    /*
+
+    Udemy solution:
+    
+    await page.goto('https://www.saucedemo.com/v1/');
+
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('asdf');
+    await page.locator('input#login-button').click();
+   
+    const errorLabel = page.locator('[data-test="error"]');
+    await expect(errorLabel).toHaveText('Epic sadface: Username and password do not match any user in this service');
+    await expect(errorLabel).toBeVisible();
+
+    */
 })
